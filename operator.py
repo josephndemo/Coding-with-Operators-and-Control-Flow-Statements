359324ee51dd4ac05ebc755a4884bdaae11f91fd
Joseph Ndemo <joseph.ndemo@student.moringaschool.com>
2026-03-05 11:45:20 -0500

    first commit

---
+++ operator.py
@@ -0,0 +1,23 @@
+# Declare variables
+num1 = 15
+num2 = 10
+
+# Arithmetic operators
+sum = num1 + num2
+difference = num1 - num2
+
+print("Sum:", sum)
+print("Difference:", difference)
+
+# Comparison operator
+if num1 > num2:
+    print("num1 is greater than num2")
+
+# Logical operator
+age = 20
+has_ticket = True
+
+if age >= 18 and has_ticket:
+    print("You are allowed to enter the event")
+else:
+    print("Access denied")
